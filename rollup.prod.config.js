ce446b6fd3be2f77b148c63048509bee228282d2
Rebecca Vest <dynamicdiner@gmail.com>
2020-06-08 18:59:40 +0000

    Undo import order

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: './src/extension.js',
