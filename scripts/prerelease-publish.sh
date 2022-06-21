d8e89a10dd326f24a6e50c39684a2dfa145925a7
Rebecca Vest <rvest@equinoxmedia.com>
2022-06-21 14:34:12 -0600

    Nit

--- scripts/prerelease-publish.sh
+++ scripts/prerelease-publish.sh
@@ -1,7 +1,4 @@
-echo $GITHUB_REF_NAME
 yarn version --new-version "$1" --no-git-tag-version
 yarn package
-# yarn vsce publish --pre-release -p "$VSCE_PAT"
-# yarn ovsx publish --pre-release -p "$OVSX_PAT"
-yarn vsce publish -p "$VSCE_PAT"
-yarn ovsx publish -p "$OVSX_PAT"
+yarn vsce publish --pre-release -p "$VSCE_PAT"
+yarn ovsx publish --pre-release -p "$OVSX_PAT"
