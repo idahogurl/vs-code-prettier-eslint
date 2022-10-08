04eae0e9e71219d45239b3fe12e7c26d2957058a
Rebecca Vest <olserebe@hotmail.com>
2022-10-07 22:26:46 -0600

    revert: :rewind: Revert esbuild "espree" replace (#99)

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const esbuild = require('esbuild');
+const textReplace = require('esbuild-plugin-text-replace');
 
 const buildEnv = {
   dev: false,
@@ -18,18 +19,18 @@ const options = {
   outfile: './dist/extension.js',
   format: 'cjs',
   platform: 'node',
-  external: ['vscode', '@typescript-eslint/parser', 'vue-eslint-parser', 'espree'],
+  external: ['vscode', '@typescript-eslint/parser', 'vue-eslint-parser'],
   bundle: true,
   sourcemap: buildEnv.dev,
   minify: buildEnv.prod,
   watch: buildEnv.watch,
-  // plugins: [
-  //   textReplace({
-  //     include: /rule-tester/,
-  //     // TODO: Until I figure out how to resolve
-  //     pattern: [['const espreePath = require.resolve("espree");', 'const espreePath = ""']],
-  //   }),
-  // ],
+  plugins: [
+    textReplace({
+      include: /rule-tester/,
+      // TODO: Until I figure out how to resolve
+      pattern: [['const espreePath = require.resolve("espree");', 'const espreePath = ""']],
+    }),
+  ],
 };
 
 esbuild.build(options).catch((err) => {
