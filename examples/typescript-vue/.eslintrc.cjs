92ff61937a3d2402d6dff02f87924bb79eb0f94a
Rebecca Vest <rvest@equinoxmedia.com>
2023-02-25 22:18:19 -0700

    chore: Update TypeScript Vue example

--- examples/typescript-vue/.eslintrc.cjs
+++ examples/typescript-vue/.eslintrc.cjs
@@ -1,15 +1,15 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
   ],
-  env: {
-    "vue/setup-compiler-macros": true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
 };
