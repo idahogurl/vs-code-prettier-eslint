3f100560f7dbf26c1b1cde6aec699c59798ed066
Rebecca Vest <rvest@equinoxmedia.com>
2023-02-26 19:10:45 -0700

    Try again

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -38,13 +38,14 @@ describe('Extension Test Suite', () => {
   });
 
   test('setting "prettierLast" makes prettier run last', async () => {
-    await helper.changeConfig(undefined, { 'vscode-prettier-eslint.prettierLast': 'true' });
     const content = fs.readFileSync(sourceFile).toString().replace('/* eslint-disable */\n', '');
     const filePath = `${basePath}/temp/test.js`;
     fs.writeFileSync(filePath, content, { overwrite: true });
     const document = await helper.openFile(filePath);
+    const config = vscode.workspace.getConfiguration('vs-code-prettier-eslint');
+    await config.update('prettierLast', 'true', vscode.ConfigurationTarget.Global);
     await vscode.commands.executeCommand('editor.action.formatDocument');
-    await helper.changeConfig(undefined, { 'vscode-prettier-eslint.prettierLast': 'false' });
+    await config.update('prettierLast', undefined, vscode.ConfigurationTarget.Global);
 
     const formatted = document.getText();
     expect(formatted).toMatchSnapshot();
