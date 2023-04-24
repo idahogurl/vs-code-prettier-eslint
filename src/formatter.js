b4ff90f2b4e3ab3e0501d19f648a39d6c5b1f2a7
Jungwoo Kim <jungwookim@jumpcloudui-MacBookPro-3.local>
2023-04-24 19:34:33 +0900

    use object, not path

--- src/formatter.js
+++ src/formatter.js
@@ -1,10 +1,11 @@
 import format from 'prettier-eslint';
+import fs from 'fs';
 
 export default function formatText({ text, filePath, extensionConfig }) {
   return format({
     text,
     filePath,
     prettierLast: extensionConfig?.prettierLast || false,
-    eslintConfig: extensionConfig?.eslintConfigPath,
+    eslintConfig: extensionConfig?.eslintConfigPath ? JSON.parse(fs.readFileSync(extensionConfig?.eslintConfigPath)) : null
   });
 }
