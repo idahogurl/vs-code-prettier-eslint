c8497619d4cdf1682b0e62236d9e0a2d30461a0a
Rebecca Vest <rvest@equinoxmedia.com>
2023-12-11 19:31:17 -0700

    docs: :memo: add jsdoc to ignore functions

--- src/ignore.js
+++ src/ignore.js
@@ -5,6 +5,11 @@ import nodeIgnore from 'ignore';
 
 const LINE_SEPARATOR_REGEX = /(\r|\n|\r\n)/;
 
+/**
+ * Returns a function that checks if a given filename is ignored based on the contents of a file.
+ * @param {string} filename - The path to the file containing the ignore patterns.
+ * @returns {Function} - A function that takes a filename as input and returns true if it is ignored, false otherwise.
+ */
 function getIsIgnored(filename) {
   const ignoreLines = fs
     .readFileSync(filename, 'utf8')
@@ -15,6 +20,13 @@ function getIsIgnored(filename) {
   return instance.ignores.bind(instance);
 }
 
+/**
+ * Checks if a file path is matched by the ignore rules defined in a specific ignore file.
+ * @param {string} filePath - The path of the file to check.
+ * @param {string} workspaceDirectory - The directory of the workspace.
+ * @param {string} ignoreFileName - The name of the ignore file.
+ * @returns {boolean} - True if the file path is matched by the ignore rules, false otherwise.
+ */
 export default function isFilePathMatchedByIgnore(filePath, workspaceDirectory, ignoreFileName) {
   const options = { cwd: path.dirname(filePath) };
   if (workspaceDirectory) {
