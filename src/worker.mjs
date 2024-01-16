cc0c41e75af2c357cab6305872103a047235200a
Roland Novák <119481813+novarolV@users.noreply.github.com>
2024-01-16 18:33:50 +0100

    fix: resolve absolute path to file url correctly (#206)
    * fix: resolve path to file url correctly
    
    * check if path absolute

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,4 +1,6 @@
+import { isAbsolute } from 'path';
 import { runAsWorker } from 'synckit';
+import { pathToFileURL } from 'url';
 
 runAsWorker(
   /**
@@ -15,7 +17,7 @@ runAsWorker(
     text, prettierEslintPath, filePath, extensionConfig,
   }) => {
     const format = /** @type {typeof import('prettier-eslint')} */ (
-      (await import(prettierEslintPath)).default
+      (await import(isAbsolute(prettierEslintPath) ? pathToFileURL(prettierEslintPath) : prettierEslintPath)).default
     );
     return format({
       text,
